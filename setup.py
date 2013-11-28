b580f83c6fcea2b9169570ed862ccd0baeaadf69
kieslee <lleelm@gmail.com>
2013-11-28 11:58:31 +0800

    init commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='xlogging',
+    version='0.1',
+    description='logging toolkit',
+    author='zhangruolin, lilo',
+    author_email='lleelm@gmail.com',
+    packages=['xlogging'],
+    )
